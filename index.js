a7bd60dc2cbb6c248829b4dbe3d55a3db07a2ff2
Al Mamun <almamunshisir770@gmail.com>
2023-05-01 11:36:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require("cors")
+const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json')
+app.use(cors())
+
+app.get('/', (req, res) => { //Add 'req' and 'res' parameters here
+  res.send('dragon is running'); //Send response using 'res' variable
+});
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+  console.log(`Dragon API is running on port:${port}`);
+});
